77f9e5c27fb2141e7ab4635bc5b602504c6fbb08
Raj Kumar Paneru <raajkumarpaneru@gmail.com>
2023-04-21 22:27:37 +0545

    add Category Resource

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\CategoryResource;
 use App\Models\Category;
 use Illuminate\Http\Request;
 
@@ -10,11 +11,11 @@ class CategoryController extends Controller
 {
     public function index()
     {
-        return Category::all();
+        return CategoryResource::collection(Category::all());
     }
 
     public function show(Category $category)
     {
-        return $category;
+        return new CategoryResource($category);
     }
 }
