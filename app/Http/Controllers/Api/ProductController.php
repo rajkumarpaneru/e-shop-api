5a521af49ea85149b4fec46bb5e0a583cbe45454
Raj Kumar Paneru <raajkumarpaneru@gmail.com>
2023-04-21 22:56:39 +0545

    paginate product to 10 products per page

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -10,7 +10,7 @@ class ProductController extends Controller
 {
     public function index()
     {
-        $products = Product::with('category')->get();
+        $products = Product::with('category')->paginate(10);
         return $products;
     }
 }
