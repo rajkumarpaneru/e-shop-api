3abeabbd242a937781cf43ada81b5dc65371abbf
Raj Kumar Paneru <raajkumarpaneru@gmail.com>
2023-04-22 15:56:38 +0545

    change existing api routes to v1 (version 1) routes

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
-use App\Http\Controllers\Api\CategoryController;
-use App\Http\Controllers\Api\ProductController;
+use App\Http\Controllers\Api\V1\CategoryController;
+use App\Http\Controllers\Api\V1\ProductController;
 use App\Models\Category;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
